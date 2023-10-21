b768dd2a6a119f3e42664d1b65903cf5ea119383
Brian <beerre20@gmail.com>
2023-10-21 11:47:56 -0300

    Agregado el renderizado del nombre de la cancion en repro

--- code.js
+++ code.js
@@ -1,14 +1,44 @@
 import playlist from "./playlists.json";
 import canciones from "./canciones.json";
 
-console.log("El módulo se ha importado correctamente.")
+console.log("El módulo se ha importado correctamente.");
 
-const inputBuscar = document.querySelector("#inputBuscar");
+// Reproductor de musica
 
+const reproductor = document.querySelector("audio");
+const botonPrimero = document.querySelectorAll(".botonPlayLista");
 
-inputBuscar.addEventListener("keyup", (e) => {
-    console.log(e.target.value)
-})
+// Funcion para que cada boton de play tome como value el nombre de su span ( Para luego matchear con el nombre de la cancion y que se reproduzca)
 
+let regex = /\w+/gi;
+botonPrimero.forEach((boton) => {
+  const botones = boton.parentNode;
+  boton.value = botones
+    .querySelector("span")
+    .textContent.match(regex)
+    .join(" ");
+});
 
+// Nombre de cancion en el reproductor
 
+const nombreCancion = document.querySelector(
+    ".nombreCancionReproductor"
+  );
+
+  const cancionNombreRepro = nombreCancion.appendChild(
+    document.createElement("span")
+  );
+
+  // Funcion que matchea el valor del boton play con el nombre de la cancion, si existe se pasa al reproductor , se muestra el nombre de la cancion en el reproductor y se pone play
+
+botonPrimero.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    for (let cancion of canciones) {
+      if (boton.value.includes(cancion.name)) {
+        reproductor.src = `${cancion.url}`;
+        reproductor.play();
+        cancionNombreRepro.textContent = `${cancion.name} - ${cancion.artist}`;
+      }
+    }
+  });
+});
